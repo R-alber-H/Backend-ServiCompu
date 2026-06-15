dc2acb288850060c6942f61d1c005a15a42cf770
Ricardo Alberco <sabado17_08_2024@outlook.com>
2026-06-15 07:42:18 -0500

    feat: creando schemas para la creacion y respuesta del modulo product

---
+++ app/schemas/product.py
@@ -0,0 +1,49 @@
+from pydantic import BaseModel, ConfigDict
+from typing import Optional, List
+
+class ProductCreate(BaseModel):
+    brand_id :int
+    supplier_id : Optional[int] = None
+    name :str
+    price :float
+    img_url :str
+    initial_stock:int
+    description: Optional[str] = None
+    category_ids:list[int]
+    
+class BrandResponse(BaseModel):
+    id: int
+    name: str
+    model_config = ConfigDict(from_attributes=True)
+    
+class SupplierResponse(BaseModel):
+    id : int
+    name:str
+    phone :str
+    model_config = ConfigDict(from_attributes=True)
+    
+class InventoryResponse(BaseModel):
+    stock : int
+    model_config = ConfigDict(from_attributes=True)
+
+class CategoryResponse(BaseModel):
+    id: int
+    name: str
+    model_config = ConfigDict(from_attributes=True)
+
+class ProductCategoryResponse(BaseModel):
+    category: CategoryResponse
+    model_config = ConfigDict(from_attributes=True)
+    
+class ProductResponse(BaseModel):
+    id:int
+    name:str
+    price:float
+    img_url:str
+    description:Optional[str] = None
+    active:bool
+    brand:BrandResponse
+    supplier: Optional[SupplierResponse] = None
+    inventory: InventoryResponse
+    product_categories : list[ProductCategoryResponse]
+    model_config = ConfigDict(from_attributes=True)
